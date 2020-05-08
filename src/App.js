62ffc4b778a2e9e63162de17893fb042ffa38858
Daniel Arcos <danielgokuarcos@yahoo.com>
2020-05-07 19:12:18 -0500

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 const App = () =>  {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
